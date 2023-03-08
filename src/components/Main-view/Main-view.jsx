9623fda54707a9b41d05c22cfce4ed52db595f84
EJQuezada <equezada@msn.com>
2023-03-08 13:18:36 +0100

    replace movie-card to Main-movie in Main-view fold

--- src/components/Main-view/Main-view.jsx
+++ src/components/Main-view/Main-view.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { MovieCard } from "../movie-card/movie-card";
+import { MovieCard } from "../Main-view/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
