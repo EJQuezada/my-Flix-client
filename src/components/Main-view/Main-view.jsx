7e9c30d19344a5705fedd1efdf573d8e78259538
EJQuezada <equezada@msn.com>
2023-03-08 10:45:18 +0100

    pass a function from Main-view to moviecard

--- src/components/Main-view/Main-view.jsx
+++ src/components/Main-view/Main-view.jsx
@@ -56,15 +56,27 @@ export const MainView = () => {
         }
     ]);
 
+    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    if (selectedMovie) {
+        return <MovieView movie={selectedMovie} />;
+    }
+
     if (movies.length === 0) {
         return <div>The list is empty!</div>;
     }
 
     return (
         <div>
-            {movies.map((movie) => {
-                return <div>{movie.title}</div>
-            })}
+            {movies.map((movie) => (
+                <MovieCard 
+                    key={movie.id} 
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                />    
+            ))}
         </div>
     );
 };
