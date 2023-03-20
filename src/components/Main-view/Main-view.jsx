d6d6738dffc0b44567af181ec800ac12e0cf187c
EJQuezada <equezada@msn.com>
2023-03-20 14:48:14 +0100

    add Row component to Main-view.jsx

--- src/components/Main-view/Main-view.jsx
+++ src/components/Main-view/Main-view.jsx
@@ -84,51 +84,34 @@ export const MainView = () => {
         });
     }, [token]);
 
-    if (!user) {
-        return (
-            <Col md={5}>
-                <LoginView
-                    onLoggedIn={(user, token) => {
-                        setUser(user);
-                        setToken(token);
-                    }}
-                />
-                or
-                <SignupView />
-            </Col>
-        );
-    };
-
-    if (selectedMovie) {
-        return (
-            <MovieView movie={selectedMovie} onBackClick={ () => 
-        setSelectedMovie (null)} />
-        );
-    }
-
-    if (movies.length === 0) {
-        return <div>The list is empty!</div>;
-    }
-
     return (
-        <div>
-            <button
-                onClick={() => {
-                    setUser(null);
-                }}
-            >
-                Logout
-            </button>
-            {movies.map((movie) => (
-                <Col className="mb-5" key={movie.id} md={3}>
-                    <MovieCard
-                        movie={movie}
-                        onMovieClick={(newSelectedMovie) => {
-                            setSelectedMovie(newSelectedMovie);
-                        }}
-                    />    
-                </Col>           
-            ))}
-        </div>
+        <Row>
+            {!user ? (
+                <>
+                    <LoginView onLoggedIn={(user) => setUser(user)} />
+                    or
+                    <SignupView />
+                </>
+            ) :selectedMovie ? (
+                <MovieView
+                    movie={selectedMovie}
+                    onBackClick={() => setSelectedMovie(null)}
+                />
+            ) : movies.length === 0 ? (
+                <div>The list is empty!</div>
+            ) : (
+                <>
+                    {movies.map((movie) => (
+                        <MovieCard
+                            key={movie.id}
+                            movie={movie}
+                            onMovieClick={(newSelectedMovie) => {
+                                setSelectedMovie(newSelectedMovie)
+                            }}
+                        />
+                    ))}
+                </>
+            )}
+        </Row>
     );
 };
