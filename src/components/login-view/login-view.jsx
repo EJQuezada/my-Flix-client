9e2a6e50e7c80eb1d9d952a233b4291d5bd9a367
EJQuezada <equezada@msn.com>
2023-03-20 12:22:13 +0100

    Change LoginView with React Bootstrap components

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -36,27 +36,31 @@ export const LoginView = ({ onLoggedIn }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input 
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
                     type="text"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     required
+                    minLength="3"
                 />
-            </label>
-            <label>
-                Password:
-                <input 
+            </Form.Group>
+            
+            <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
                     type="password"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)} 
+                    onChange={(e) => setPassword(e.target.vlue)}
                     required
                 />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     );
 };
 
