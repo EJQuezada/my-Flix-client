49336f49a55496dbf9c1ef4036b248c561e029bd
EJQuezada <equezada@msn.com>
2023-03-21 09:59:42 +0100

    fix typo in Password function

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -53,7 +53,7 @@ export const LoginView = ({ onLoggedIn }) => {
                 <Form.Control
                     type="password"
                     value={password}
-                    onChange={(e) => setPassword(e.target.vlue)}
+                    onChange={(e) => setPassword(e.target.value)}
                     required
                 />
             </Form.Group>
