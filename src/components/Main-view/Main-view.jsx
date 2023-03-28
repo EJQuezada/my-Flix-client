9fc7dbf0cace5a6d178ab078f9ff7f88ee40c88f
EJQuezada <equezada@msn.com>
2023-03-29 00:49:46 +0200

    add Link funct, movies prop, and toggle favorite

--- src/components/Main-view/Main-view.jsx
+++ src/components/Main-view/Main-view.jsx
@@ -148,7 +148,7 @@ export const MainView = () => {
                                     <>
                                     {movies.map((movie) => (
                                         <Col className="mb-4" key={movie.id} md={3}>
-                                            <MovieCard movie={movie} />
+                                            <MovieCard movie={movie} toggleFavorite={toggleFavorite} />
                                         </Col>
                                     ))}
                                     </>
