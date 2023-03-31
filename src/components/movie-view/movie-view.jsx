b2588b9e27c313ae33d2c6c3f182d858daf53f84
EJQuezada <equezada@msn.com>
2023-03-31 13:52:13 +0200

    fix PropTypes function

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import {PropTypes} from "prop-types";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import "./movie-view.scss";
