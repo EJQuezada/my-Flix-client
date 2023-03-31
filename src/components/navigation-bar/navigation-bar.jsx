eaa0a097f3782a20845928a0c7807dadf70384ff
EJQuezada <equezada@msn.com>
2023-03-31 10:39:49 +0200

    clean up code in Navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from react-router-dom;
+import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
     return (
@@ -13,8 +13,12 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                     <Nav className="me-auto">
                         {!user && (
                             <>
-                                <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                                <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+                                <Nav.Link as={Link} to="/login">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/signup">
+                                    Signup
+                                </Nav.Link>
                             </>
                         )}      
                         {user && (
