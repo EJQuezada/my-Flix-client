ecf612d4a3f582a4f11ec67f4eaaa02bd8e3d56d
EJQuezada <equezada@msn.com>
2023-04-18 12:39:54 +0200

    add user reducer to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./reducers/movies";
+import userReducer from "./reducers/user";
 
 export const store = configureStore({
-    reducer: { movies: moviesReducer }
+    reducer: { movies: moviesReducer, user: userReducer }
 });
 
 export const { setMovies } = moviesSlice.actions;
