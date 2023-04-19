d0d0cdcef2b3a888199da1c43844966df701f2d9
EJQuezada <equezada@msn.com>
2023-04-19 11:19:11 +0200

    update store.js with reducer functions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import moviesReducer from "./reducers/movies";
+import userReducer from "./reducers/user/user";
 
-export const store = configureStore({});
+export const store = configureStore({
+    reducer: { movies: moviesReducer, user: userReducer }
+});
 
 export const { setMovies } = moviesSlice.actions;
 
