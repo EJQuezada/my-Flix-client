b0fff7a8d4d5e4c0fce4014b4e693d37adf8f0a3
EJQuezada <equezada@msn.com>
2023-04-26 14:32:02 +0200

    add import React function to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/Main-view/Main-view";
 import Container from "react-bootstrap/Container";
